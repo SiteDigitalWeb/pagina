f8e8be4672bfa93702310eb0778c1b2ece02d29f
SiteDigitalOficial <144865258+SiteDigitalOficial@users.noreply.github.com>
2026-01-01 11:22:42 -0500

    Ajustes PWA
    Ajustes PWA

--- src/Http/PwaManifestController.php
+++ src/Http/PwaManifestController.php
@@ -384,13 +384,6 @@ public function manifest()
     ->header('Content-Type', 'application/manifest+json')
     ->header('Cache-Control', 'no-cache, no-store, must-revalidate');
 }
-
- 
-
-
-
-
-
     
 }
 
